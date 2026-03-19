406b9cb20a6a34acbc83a7a1433506895812ee59
nuVooo <sebastian.angelone@googlemail.com>
2026-03-19 15:47:33 +0100

    feat: add MailerModule to AuthModule and OrganizationsModule for email functionality

--- backend/src/modules/auth/auth.module.ts
+++ backend/src/modules/auth/auth.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { SetupController } from './setup.controller';
+import { PrismaModule } from '../../shared/prisma.module';
+import { MailerModule } from '../../shared/mailer.module';
 
 @Module({
+  imports: [PrismaModule, MailerModule],
   controllers: [AuthController, SetupController],
   providers: [AuthService],
 })
--- backend/src/modules/organizations/organizations.module.ts
+++ backend/src/modules/organizations/organizations.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { OrganizationsController } from './organizations.controller';
 import { OrganizationsService } from './organizations.service';
 import { PrismaModule } from '../../shared/prisma.module';
+import { MailerModule } from '../../shared/mailer.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, MailerModule],
   controllers: [OrganizationsController],
   providers: [OrganizationsService],
 })
